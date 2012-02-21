25e2987a19791c29e3c08898efaa1f6b33f2ea5a
James Adney <jfadney@gmail.com>
2012-02-21 12:51:22 -0800

    initial commit

---
+++ screensaver.py
@@ -0,0 +1,110 @@
+import sys
+import pygame
+from pygame.locals import *
+import util
+
+BLACK = (0, 0, 0)
+FPS = 30
+PICTURE_DIR = "/home/james/Pictures/wallpapers"
+UPDATE_PICTURE_EVENT = USEREVENT + 1
+PICTURE_DELAY = 10000   # Time between pictures in milliseconds
+
+
+# TODO: Can this be improved to make it more easily imported by other 
+#       applications?
+#
+# Class to encapsulate the entire application
+class Screensaver():
+    """..."""
+
+    def __init__(self):
+        """..."""
+        # TODO: Is this the best place to call this
+        pygame.init()
+        
+        self._set_screen_size()
+        self._initialize_screen()
+        self._initialize_clock()
+        
+        self.pics = util.Pictures()
+        
+        self.run()
+        
+    def _set_screen_size(self):
+        """..."""
+    
+        # get size of fullscreen display into screen_x, screen_y
+        modes = pygame.display.list_modes()    # defaults to fullscreen
+        modes.sort()                           # largest goes last
+        self.screen_x, self.screen_y = modes[-1]
+        self.screen_ratio = float(self.screen_x) / self.screen_y
+        
+    def _initialize_screen(self):
+        """..."""
+        
+        self.screen = pygame.display.set_mode((self.screen_x, self.screen_y),
+                                              pygame.FULLSCREEN )
+        pygame.display.set_caption('Slideshow Screensaver')
+        
+    def _initialize_clock(self):
+        self.fps_clock = pygame.time.Clock()
+        
+    def show_picture(self):
+        random_pic = self.pics.get_random()
+        image = pygame.image.load(random_pic).convert()
+        
+        # Resize image to fit on screen
+        image_x, image_y = image.get_size()
+        print "Original Image Size: {0}x{1}".format(image_x, image_y)
+        image_ratio = float(image_x) / image_y
+        if image_ratio >= self.screen_ratio:
+            resized_x = self.screen_x
+            resized_y = (resized_x * image_y) / image_x
+        else:
+            resized_y = self.screen_y
+            resized_x = (resized_y * image_x) / image_y
+        print "Scaled Image Size: {0}x{1}".format(resized_x, resized_y)
+        image = pygame.transform.scale(image, (resized_x, resized_y))
+        
+        # Center image
+        image_rect = image.get_rect()
+        image_rect.center = self.screen.get_rect().center
+        
+        self.screen.blit(image, image_rect)
+   
+        pygame.display.update()
+        
+    def run(self):
+        
+        # Load first picture and start timer
+        self.show_picture()
+        pygame.time.set_timer(UPDATE_PICTURE_EVENT, PICTURE_DELAY)
+        
+        # Hide mouse cursor
+        pygame.mouse.set_visible(False)
+        
+        # Clear initial mouse event
+        pygame.event.clear()
+        
+        while True:
+            event = pygame.event.poll()
+            
+            if event.type == UPDATE_PICTURE_EVENT:
+                self.show_picture()
+                
+            elif event.type != pygame.NOEVENT:
+                self.quit()
+                
+            self.fps_clock.tick(FPS)
+                
+    def quit(self):
+        
+        # Close screensaver quickly
+        pygame.display.quit()
+        
+        pygame.quit()
+        sys.exit()
+            
+              
+if __name__ == "__main__":
+    app = Screensaver()
---
+++ util.py
@@ -0,0 +1,27 @@
+import os.path
+import random
+import mimetypes
+
+class Pictures():
+
+    def __init__(self, pics_root=os.path.expanduser("~/Pictures")):
+        
+        self.pics_root = pics_root
+        
+        self.generate_list()
+    
+    def generate_list(self):
+        
+        self.pics = []
+        for d in os.walk(self.pics_root):
+            directory = d[0]
+            
+            for filename in d[2]:
+                filepath = os.path.join(directory, filename)
+                if mimetypes.guess_type(filepath)[0] == 'image/jpeg':
+                    self.pics.append(filepath)
+                    
+    def get_random(self):
+    
+        random_pic = random.choice(self.pics)
+        return random_pic
