0b44ea02a87c0bb924a9a3b53cbdce8ff36d6523
James Adney <jfadney@gmail.com>
2012-02-24 21:00:45 -0800

    tweaked setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ from setuptools import setup, find_packages
 
 setup(name='Slideshow Screensaver',
       version='0.1',
-      summary='Slideshow screensaver for Gnome based on Pygame',
+      description='Slideshow screensaver for Gnome based on Pygame',
       keywords=['slideshow', 'screensaver', 'gnome'],
       author=u'James Adney',
       author_email='jfadney@gmail.com',
-      home_page='https://github.com/jamesadney/slideshow-screensaver',
+      url='https://github.com/jamesadney/slideshow-screensaver',
       license='GPL3',
       packages=find_packages(),
       scripts=["watch-idle.py", "screensaver.py"],
-      install_requires=["distribute",
-                        "pygame",
-                        "dbus-python",
-                        "pygir-ctypes"])
+
+      # Also requires dbus-python and python-gobject
+      install_requires=["distribute", "pygame"])
