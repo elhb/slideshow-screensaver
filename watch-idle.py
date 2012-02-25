26d93051bcb1552767a0a7d27226b6af4656f6a9
James Adney <jfadney@gmail.com>
2012-02-24 19:01:19 -0800

    removed extra whitespace chars from watch-idle.py

--- watch-idle.py
+++ watch-idle.py
@@ -7,9 +7,9 @@ import checkidle
 
 
 class System():
-    
+
     def __init__(self):
-        
+
         self.SCREENSAVER_DELAY = 60000   # milliseconds
         self.CLOCK_TIMEOUT = 5000        # time between checks in milliseconds
         self.idle = False
@@ -20,13 +20,13 @@ class System():
     def check_idle(self, delay):
         self.idle = checkidle.is_idle(delay)
         return self.idle
-    
+
     def check_locked(self):
         self.locked = checklock.is_locked()
         return self.locked
-        
+
     def _check_screensaver(self):
-    
+
         try:
             retcode = self.screensaver.poll()
             if retcode is not None:
@@ -36,40 +36,40 @@ class System():
         except AttributeError:
             print "Screensaver not started yet"
             self.screensaver_on = False
-        
+
     def start_screensaver(self):
         print "Starting screensaver"
         process = subprocess.Popen(["screensaver.py"])
-        
+
         return process
-    
+
     def manage_screensaver(self):
-        
+
         # Use decorators to call these when attributes are read?
         self.check_idle(self.SCREENSAVER_DELAY)
         self.check_locked()
-        
+
         # TODO: Have screensaver emit dbus signal when it dies, or use
         #       idle time to determine this?
         #
         # Know if screensaver killed itself
         self._check_screensaver()
-        
+
         print "Screensaver callback"
         if self.screensaver_on and not self.locked:
             return True
-        
+
         elif self.screensaver_on and self.locked:
             print "Killing screensaver"
             self.screensaver.terminate()
             self.screensaver_on = False
-        
+
         elif self.idle and not self.locked:
             self.screensaver = self.start_screensaver()
             self.screensaver_on = True
-            
+
         return True
-    
+
 
 if __name__ == "__main__":
 
@@ -80,4 +80,3 @@ if __name__ == "__main__":
         loop.run()
     except KeyboardInterrupt:
         loop.quit()
-    
