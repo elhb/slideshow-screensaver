16ce48275de1277ebb809e82346a368bd983c0e8
James Adney <jfadney@gmail.com>
2012-02-24 18:58:14 -0800

    moved modules into package

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get script directory
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
-xautolock -time 1 -locker "python2 $DIR/screensaver.py"
