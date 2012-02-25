15320ffd363ef9b9707f72320699f0f3dc9b383b
James Adney <jfadney@gmail.com>
2012-02-24 22:15:52 -0800

    added license boilerplate and file

--- slidesaver/checkidle.py
+++ slidesaver/checkidle.py
@@ -1,3 +1,18 @@
+# Slideshow Screensaver
+# Copyright (C) 2012 James Adney
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import ctypes
 import os
 
@@ -9,8 +24,8 @@ class XScreenSaverInfo(ctypes.Structure):
               ('since', ctypes.c_ulong), # milliseconds
               ('idle', ctypes.c_ulong), # milliseconds
               ('event_mask', ctypes.c_ulong)] # events
-  
-  
+
+
 def get_idle_time():
 
     xlib = ctypes.cdll.LoadLibrary('libX11.so')
@@ -18,8 +33,8 @@ def get_idle_time():
     xss = ctypes.cdll.LoadLibrary('libXss.so.1')
     xss.XScreenSaverAllocInfo.restype = ctypes.POINTER(XScreenSaverInfo)
     xssinfo = xss.XScreenSaverAllocInfo()
-    xss.XScreenSaverQueryInfo(display, 
-                              xlib.XDefaultRootWindow(display), 
+    xss.XScreenSaverQueryInfo(display,
+                              xlib.XDefaultRootWindow(display),
                               xssinfo)
 
     idle_time = xssinfo.contents.idle
@@ -29,10 +44,10 @@ def get_idle_time():
 def is_idle(delay):
         idle_time = get_idle_time()
         print idle_time
-        
+
         if idle_time >= delay:
             idle = True
         else:
             idle = False
-            
+
         return idle
--- slidesaver/checklock.py
+++ slidesaver/checklock.py
@@ -1,12 +1,25 @@
-#TODO: Expand to work with xscreensaver, KDE, ...
+# Slideshow Screensaver
+# Copyright (C) 2012 James Adney
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import dbus
 
 def is_locked():
     session_bus = dbus.SessionBus()
-    dbus_object  = session_bus.get_object("org.gnome.ScreenSaver","/")
+    dbus_object = session_bus.get_object("org.gnome.ScreenSaver", "/")
     status = dbus_object.GetActive(dbus_interface="org.gnome.ScreenSaver")
-    
+
     if status == 0:
         return False
     else:
