a446e9839489a645a51cb456cfe1063c237bae3c
James Adney <jfadney@gmail.com>
2012-03-04 22:10:37 -0800

    sloppy code with fadein/fadeout working

--- screensaver.py
+++ screensaver.py
@@ -8,6 +8,7 @@ import Image
 from slidesaver import util
 
 PICTURE_DIR = os.path.expanduser("~/Pictures")
+CROP = True
 
 class AppWindow(pyglet.window.Window):
 
@@ -24,8 +25,29 @@ class AppWindow(pyglet.window.Window):
         self.screen_ratio = float(self.screen_x) / self.screen_y
         self.center_x = self.screen_x // 2
         self.center_y = self.screen_y // 2
+        self.transitioning = False
+        self.old_picture = None
+        self.picture = None
 
         self.load_new_image()
+
+    def increase_opacity(self, dt):
+        if self.picture.opacity <= 255 and self.transitioning:
+            if self.picture.opacity + 2 > 255:
+                self.picture.opacity = 255
+                if self.old_picture:
+                    self.old_picture.opacity = 0
+            else:
+                self.picture.opacity += 2
+                if self.old_picture:
+                    self.old_picture.opacity -= 2
+        else:
+            self.transitioning = False
+
+    def zoom(self, dt):
+        self.picture.scale += .05 * dt
+        if self.old_picture:
+            self.old_picture.scale += .05 * dt
 
     def load_new_image(self, dt=None):
         random_pic = self.pics.get_random()
@@ -36,12 +58,20 @@ class AppWindow(pyglet.window.Window):
         image_x, image_y = temp_image.size
         image_ratio = float(image_x) / image_y
 
-        if image_ratio >= self.screen_ratio:
-            resized_x = self.screen_x
-            resized_y = (resized_x * image_y) // image_x
+        if CROP:
+            if image_ratio >= self.screen_ratio:
+                resized_y = self.screen_y
+                resized_x = (resized_y * image_x) // image_y
+            else:
+                resized_x = self.screen_x
+                resized_y = (resized_x * image_y) // image_x
         else:
-            resized_y = self.screen_y
-            resized_x = (resized_y * image_x) // image_y
+            if image_ratio >= self.screen_ratio:
+                resized_x = self.screen_x
+                resized_y = (resized_x * image_y) // image_x
+            else:
+                resized_y = self.screen_y
+                resized_x = (resized_y * image_x) // image_y
 
         temp_image = temp_image.resize((resized_x, resized_y))
         raw_image = temp_image.tostring()
@@ -56,10 +86,15 @@ class AppWindow(pyglet.window.Window):
         self.image.anchor_x = self.image.width // 2
         self.image.anchor_y = self.image.height // 2
 
+        if self.picture:
+            self.old_picture = self.picture
+
         self.picture = pyglet.sprite.Sprite(self.image)
 
         # Move sprite to center
         self.picture.set_position(self.center_x, self.center_y)
+        self.picture.opacity = 0
+        self.transitioning = True
 
 
 def quit(window):
@@ -70,12 +105,16 @@ def quit(window):
 if __name__ == "__main__":
 
     app = AppWindow()
+    pyglet.clock.schedule_interval(app.increase_opacity, 1.0 / 120)
     pyglet.clock.schedule_interval(app.load_new_image, 10)
+#    pyglet.clock.schedule_interval(app.zoom, 1.0 / 30)
 
     @app.event
     def on_draw():
         app.clear()
         app.picture.draw()
+        if app.old_picture:
+            app.old_picture.draw()
 
     @app.event
     def on_key_press(symbol, modifiers):
