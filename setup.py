334ba4f3aeaf128985ed5f74fdb881409be25b10
James Adney <jfadney@gmail.com>
2012-03-05 08:42:33 -0800

    use xautolock by default.  removed watch-idle.py.

--- setup.py
+++ setup.py
@@ -6,16 +6,14 @@ with open('README') as f:
     long_description = f.read()
 
 setup(name='slideshow-screensaver',
-      version='0.1',
-      description='Slideshow screensaver for Gnome based on Pygame',
-      keywords=['slideshow', 'screensaver', 'gnome'],
+      version='0.2',
+      description='Slideshow screensaver for GNU/Linux using OpenGL',
+      keywords=['slideshow', 'screensaver', 'opengl'],
       author='James Adney',
       author_email='jfadney@gmail.com',
       url='https://github.com/jamesadney/slideshow-screensaver',
       license='GPL3',
       packages=find_packages(),
-      scripts=["watch-idle.py", "screensaver.py"],
+      scripts=["slidesaver-daemon", "screensaver.py"],
       long_description=long_description,
-
-      # Also requires dbus-python and python-gobject
       install_requires=["distribute", "pyglet", "PIL"])
